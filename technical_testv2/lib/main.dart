30aa40057361e47adb99a61b06e2a3f410086962
sakutox <42874705+sakutox@users.noreply.github.com>
2022-06-09 15:42:32 -0500

    testing changes trought cmd

--- technical_testv2/lib/main.dart
+++ technical_testv2/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('Material App Bar x'),
         ),
         body: Center(
           child: Container(
